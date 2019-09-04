f7944a916aeac116deeb37d048ddd3ef940075a8
mayingjie <1186806862@qq.com>
2019-09-04 08:59:12 +0800

    创建项目

---
+++ day4/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+        <span v-for="(item,index) in list" :key="index">{{item}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        list:["北京","上海","天津","重庆","北京"]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  *{
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    list-style: none;
+    outline: none;
+    user-select: none;
+    appearance: none;
+    box-sizing: border-box;
+  }
+  html,body,#app{
+    width: 100%;
+    height: 100%;
+  }
+</style>
